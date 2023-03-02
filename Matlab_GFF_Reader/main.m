f1ccd44bf0219ed2e7883b99d0e1495b1edfe822
hBTPS <91115249+hBTPS@users.noreply.github.com>
2023-03-02 18:29:45 +0100

    Update main.m

--- Matlab_GFF_Reader/main.m
+++ Matlab_GFF_Reader/main.m
@@ -1,12 +1,12 @@
 clear all; close all;
 
-MiniSAR = dir('MiniSAR\*.gff');
-mkdir('MiniSAR jpg');
+MiniSAR = dir('..\MiniSAR_GFF\*.gff');
+mkdir('..\MiniSAR_JPG');
 
 for k=1:length(MiniSAR)
     % pathname = 'MiniSAR20050519p0001image008.gff';
     pathname = MiniSAR(k).name;
-    [Image, Header, fname] = load_gff_1_8b(strcat('MiniSAR\', pathname));
+    [Image, Header, fname] = load_gff_1_8b(strcat('..\MiniSAR_GFF\', pathname));
     
     if Header.BytesPerPixel>1
         im_qp = sqrt(abs(Image));
@@ -17,7 +17,7 @@ for k=1:length(MiniSAR)
     im_qp = 255*im_qp/max(max(im_qp));
     im_qp = round(min(4*im_qp,255));
     
-    imwrite(ind2rgb(im_qp, gray(256)), strcat('MiniSAR jpg\', pathname(1:end-4), '.jpg'));
+    imwrite(ind2rgb(im_qp, gray(256)), strcat('..\MiniSAR_JPG\', pathname(1:end-4), '.jpg'));
 
     subplot(5, 4, k); 
     image(im_qp); 
